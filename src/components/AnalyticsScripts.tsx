ba5cc1ace02d65131ce0fca9d52e1722cd2f7e95
inunogegup63-sys <inunogegup63-sys@users.noreply.github.com>
2026-03-31 11:23:14 +0300

    feat: update Yandex Metrika init params (ssr, ecommerce, referrer) + noscript pixel

--- src/components/AnalyticsScripts.tsx
+++ src/components/AnalyticsScripts.tsx
@@ -35,14 +35,21 @@ export default function AnalyticsScripts() {
     <>
       {/* Yandex.Metrika */}
       {a.yandexMetrika && (
-        <Script id="yandex-metrika" strategy="afterInteractive">
-          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r)return;}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");
-          ym(${a.yandexMetrika},"init",{clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});`}
-        </Script>
+        <>
+          <Script id="yandex-metrika" strategy="afterInteractive">
+            {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r)return;}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");
+            ym(${a.yandexMetrika},"init",{ssr:true,webvisor:true,clickmap:true,trackLinks:true,accurateTrackBounce:true,ecommerce:"dataLayer",referrer:document.referrer,url:location.href});`}
+          </Script>
+          <noscript>
+            <div>
+              <img src={`https://mc.yandex.ru/watch/${a.yandexMetrika}`} style={{ position: "absolute", left: "-9999px" }} alt="" />
+            </div>
+          </noscript>
+        </>
       )}
 
       {/* Google Analytics GA4 */}
